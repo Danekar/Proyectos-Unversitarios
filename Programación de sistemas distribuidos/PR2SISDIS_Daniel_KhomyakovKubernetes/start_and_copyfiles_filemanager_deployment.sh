afbf655f8779419610ee927989e881e32c175d39
Danekar <56341492+Danekar@users.noreply.github.com>
2022-10-05 10:05:54 +0200

    PDSD

---
+++ Programación de sistemas distribuidos/PR2SISDIS_Daniel_KhomyakovKubernetes/start_and_copyfiles_filemanager_deployment.sh
@@ -0,0 +1,6 @@
+#Daniel Khomyakov y Raul Martinez Llorente
+
+kubectl apply -f filemanager.yaml
+sleep 10
+fmpod=$(kubectl get pods | grep 'filemanager' | awk '{print $1}'| head -n 1)
+kubectl cp filemanager/dirpruebabackup/. $fmpod:/dirprueba
